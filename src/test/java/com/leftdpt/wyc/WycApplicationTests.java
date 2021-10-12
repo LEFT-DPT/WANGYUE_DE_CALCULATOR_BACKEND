ca6a95621216ce71f62cb7895ab6b9f9b280a812
geys <geys@tsintergy.com>
2021-10-12 11:26:53 +0800

    feat 初始化项目

---
+++ src/test/java/com/leftdpt/wyc/WycApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leftdpt.wyc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WycApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
